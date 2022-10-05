1752eb190fb5ed97baca57257fc96273f6fc2c0e
Danil Maksimenko I-03 <dm76861@gmail.com>
2022-10-05 22:32:39 +0700

    Теперь точно третье готово.

--- 58.1.php
+++ 58.1.php
@@ -21,17 +21,9 @@
 
 	class File implements iFile {
 		public function __construct($filePath = '') {
-			if ($filePath != '') {
-				$this->path = "./$filePath";
-				
-				if (!file_exists(dirname($this->path))) {
-					mkdir(dirname($this->path), 0777, true);
-				}
+			$this->path = "./$filePath";
 
-				if (!file_exists($filePath)) {
-					file_put_contents($filePath, '');
-				}
-			}
+			$this->specialHandler();
 		}
 
 		public function getPath() {
@@ -66,9 +58,9 @@
 		public function appendText($text) {
 			file_put_contents($this->path, $text, FILE_APPEND);
 		}
-		//Максименко И-03
+		// Максименко И-03
 		public function copy($copyPath) {
-			copy($this->path, $copyPath);
+			copy($this->path, dirname($this->path).'/'.$copyPath);
 		}
 
 		public function delete() {
@@ -78,16 +70,36 @@
 		public function rename($newName) {
 			// Аргумент - не путь!!!
 
-			rename($this->path, dirname($this->path)."newName");
+			rename($this->path, dirname($this->path)."/$newName");
+
+			$this->path = dirname($this->path).'/'.$newName;
 		}
 		
 		public function replace($newPath) {
-			rename($this->path, $newPath);
-			$this->path = $newPath;
+			rename($this->path, dirname($this->path)."/".$newPath);
+			$this->path = dirname($this->path).'/'.$newPath;
+		}
+
+		private function specialHandler() {
+		// Специальный обработчик. Создаёт папки/файлы,
+		// описанные в пути, если их ещё нет.
+			if ($this->path != '') {
+				
+				if (!file_exists(dirname($this->path))) {
+					mkdir(dirname($this->path), 0777, true);
+				}
+
+				if (!file_exists($this->path)) {
+					file_put_contents($this->path, '');
+				}
+			}
 		}
 	}
 
 	$php = new File('dir/oneMoreDir/phpText.txt');
+
+	$php->setText('Setted text!');
+
 	echo $php->getPath().'<br>';
 	echo $php->getDir().'<br>';
 	echo $php->getExt().'<br>';
@@ -105,7 +117,22 @@
         $php->appendText('Appended').'<br>';
 	echo $php->getText().'<br>';
 
-        $php->copy('copied.txt.').'<br>';  
+        $php->copy('copied.txt').'<br>';  
         // $php->delete().'<br>';         
-        // echo $php->rename('renamed').'<br>'; 
+        // echo $php->rename('renamed.txt').'<br>'; 
+
+        $php->rename('renamed2.txt'); 
+        $php->replace('replaced2.txt');
+	
+	$php->copy('first.txt');
+	$php->copy('second.txt');
+	$php->copy('third.txt');
+
+	$php->replace('rpl.txt');
+	echo '';
+	$php->appendText('app');
+
+	echo 'После $php->rename имя файла изменилось на '. $php->getName().', а после $php->replace полноостью изменился его путь, так что
+		удаляется '.$php->getPath().'...';
+        $php->delete();         
 ?>
